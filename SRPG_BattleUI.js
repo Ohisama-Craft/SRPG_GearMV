053e7fb3dd6844b8f9add6afe994faa47a0d059e
Takumi Ariake <99699015+Ohisama-Craft@users.noreply.github.com>
2023-08-10 13:16:03 +0900

    Update SRPG_BattleUI.js 1.04Q

--- SRPG_BattleUI.js
+++ SRPG_BattleUI.js
@@ -10,6 +10,9 @@
 * @author SRPG Team
 *
 * @help
+* Copyright (c) 2020 SRPG Team. All rights reserved.
+* Released under the MIT license.
+* ===================================================================
 * My (RyanBram) simple plugin for adjusting RPG Maker MV UI (menu)
 * to make it more unique for SRPG Battle
 * edited by Shoukang to support battlePrepare plugin compatibility
@@ -21,6 +24,9 @@
 * @author SRPG Team
 *
 * @help
+* Copyright (c) 2020 SRPG Team. All rights reserved.
+* Released under the MIT license.
+* ===================================================================
 * RyanBram氏による、メニュー画面をSRPGバトル向けUIに変更するプラグイン
 * Shoukang氏のbattlePrepare pluginとの競合対策あり
 * おひさまクラフトによる改変あり
