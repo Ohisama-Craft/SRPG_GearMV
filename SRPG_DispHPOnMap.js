95814fbaa4f26252b61bb2492b0264c9de1ad738
Takumi Ariake <99699015+Ohisama-Craft@users.noreply.github.com>
2024-09-10 13:54:57 +0900

    Update SRPG_DispHPOnMap.js 1.18Q

--- SRPG_DispHPOnMap.js
+++ SRPG_DispHPOnMap.js
@@ -167,6 +167,14 @@
 	};
 
 //=============================================================================
+// Game_BattlerBase
+//=============================================================================
+    // UX_Windowsで再定義する
+    Game_BattlerBase.prototype.srpgHideHpMp = function() {
+		return false;
+	};
+
+//=============================================================================
 // Game_Interpreter
 //=============================================================================
     // 指定したイベントIDのユニットを離脱させる。
@@ -195,16 +203,13 @@ Window_DispHPGauge.prototype = Object.create(Window_Base.prototype);
 Window_DispHPGauge.prototype.constructor = Window_DispHPGauge;
 
 Window_DispHPGauge.prototype.initialize = function() {
-    //this._opacitySpeed = 255 / Yanfly.Param.VHGGaugeDuration;
     this._dropSpeed = 0;
     this._visibleCounter = 0;
-	// 
     Window_Base.prototype.initialize.call(this, -44, -48, 88, 48);
     this._battler = null;
     this._requestRefresh = false;
     this._currentHpValue = 0;
     this._displayedValue = 0;
-    //this.contentsOpacity = 0;
     this.opacity = 0;
 };
 
@@ -223,24 +228,12 @@ Window_DispHPGauge.prototype.update = function() {
 
 Window_DispHPGauge.prototype.updateWindowAspects = function() {
     this.updateWindowSize();
-    //this.updateWindowPosition();
     this.updateOpacity();
     this.updateHpPosition();
     this.updateRefresh();
 };
 
 Window_DispHPGauge.prototype.updateWindowSize = function() {
-    //var spriteWidth = this._battler.hpGaugeWidth();
-/*
-    var spriteWidth = 48;
-	var width = spriteWidth + this.standardPadding() * 2;
-    width = Math.min(width, Graphics.boxWidth + this.standardPadding() * 2);
-    var height = Math.max(this.lineHeight(), this.gaugeHeight() + 4);
-    height += this.standardPadding() * 2;
-    if (width === this.width && height === this.height) return;
-    this.width = width;
-    this.height = height;
-*/
 	var width = 48 + this.standardPadding() * 2;
 	var height = this.lineHeight() + this.standardPadding() * 2;
 	if (width === this. width && height === this.height) return;
@@ -248,35 +241,8 @@ Window_DispHPGauge.prototype.updateWindowSize = function() {
 	this.height = height;
     this.createContents();
     this._requestRefresh = true;
-    //this.makeWindowBoundaries();
 };
-/*
-Window_DispHPGauge.prototype.makeWindowBoundaries = function() {
-    if (!this._requestRefresh) return;
-    this._minX = -1 * this.standardPadding();
-    this._maxX = Graphics.boxWidth - this.width + this.standardPadding();
-    this._minY = -1 * this.standardPadding();
-    this._maxY = Graphics.boxHeight - this.height + this.standardPadding();
-    this._maxY -= SceneManager._scene._statusWindow.height;
-};
-*/
-/*
-Window_DispHPGauge.prototype.updateWindowPosition = function() {
-    if (!this._battler) return;
-    var battler = this._battler;
-    this.x = battler.spritePosX();
-    this.x -= Math.ceil(this.width / 2); 
-    this.x = this.x.clamp(this._minX, this._maxX);
-    this.y = battler.spritePosY();
-    if (Yanfly.Param.VHGGaugePos) {
-      this.y -= battler.spriteHeight();
-    } else {
-      this.y -= this.standardPadding();
-    }
-    this.y = this.y.clamp(this._minY, this._maxY);
-    this.y += Yanfly.Param.VHGBufferY;
-};
-*/
+
 Window_DispHPGauge.prototype.updateOpacity = function() {
     if (this.isShowWindow()) {
       this.contentsOpacity += 32;
@@ -289,25 +255,14 @@ Window_DispHPGauge.prototype.isShowWindow = function() {
 	if (!this._battler) return false;
 	if (this._battler.isDead()) return false;
 	return true;
-    //if (!this._battler.isAppeared()) return false;
-    //if (!this._battler.hpGaugeVisible()) return false;
-/*
-    if (Yanfly.Param.VHGAlwaysShow && !this._battler.isDead()) return true;
-    if (this._currentHpValue !== this._displayedValue) return true;
-    if (this._battler.isSelected()) return true;
-    --this._visibleCounter;
-    return this._visibleCounter > 0;
-*/
 };
 
 Window_DispHPGauge.prototype.updateHpPosition = function() {
     if (!this._battler) return;
     if (this._currentHpValue !== this._battler.hp) {
-      //this._visibleCounter = Yanfly.Param.VHGGaugeDuration;
       this._visibleCounter = 10;
       this._currentHpValue = this._battler.hp;
       var difference = Math.abs(this._displayedValue - this._battler.hp);
-      // this._dropSpeed = Math.ceil(difference / Yanfly.Param.VHGGaugeDuration);
       this._dropSpeed = Math.ceil(difference / 10);
     }
     this.updateDisplayCounter();
@@ -338,27 +293,10 @@ Window_DispHPGauge.prototype.refresh = function() {
     if (!this._battler) return;
     if (this._battler.isDead()) return;
     this._requestRefresh = false;
-    //var wy = this.contents.height - this.lineHeight();
     var wy = this.contents.height - this.lineHeight();
 	var ww = this.contents.width;
     this.drawActorHp(this._battler, 0, wy, ww);
 };
-/*
-Window_DispHPGauge.prototype.gaugeBackColor = function() {
-	return this.textColor(7);
-    return this.textColor(this._battler.hpGaugeBackColor());
-};
-
-Window_DispHPGauge.prototype.hpGaugeColor1 = function() {
-	return this.textColor(11);
-    return this.textColor(this._battler.hpGaugeColor1());
-};
-
-Window_DispHPGauge.prototype.hpGaugeColor2 = function() {
-	return this.textColor(12);
-    return this.textColor(this._battler.hpGaugeColor2());
-};
-*/
 
 Window_DispHPGauge.prototype.hpGaugeColor1 = function() {
 	if (!this._battler || this._battler.isActor()) {
@@ -380,51 +318,12 @@ Window_DispHPGauge.prototype.drawActorHp = function(actor, x, y, width) {
     width = width || 186;
     var color1 = this.hpGaugeColor1();
     var color2 = this.hpGaugeColor2();
-    var rate = this._displayedValue / actor.mhp;
-/*
-    if (Imported.YEP_AbsorptionBarrier && actor.barrierPoints() > 0) {
-      ww = this.drawBarrierGauge(actor, x, y, width);
+    if (actor.srpgHideHpMp()) {
+        var rate = 1.0;
     } else {
-      this.drawGauge(x, y, width, rate, color1, color2);
-	console.log("x:"+x+", y:"+y+", w:"+width+", rate:"+rate);
+        var rate = this._displayedValue / actor.mhp;
     }
-*/	
 	this.drawGauge(x, y, width, rate, color1, color2);
-/*
-    if (Yanfly.Param.VHGShowHP) {
-      this.changeTextColor(this.systemColor());
-      this.drawText(TextManager.hpA, x, y, 44);
-    }
-*/
-/*
-    if (Yanfly.Param.VHGShowValue) {
-      var val = this._displayedValue
-      var max = actor.mhp;
-      var w = width;
-      var color = this.hpColor(actor);
-      this.drawCurrentAndMax(val, max, x, y, w, color, this.normalColor());
-    }
-*/
 };
-/*
-Window_DispHPGauge.prototype.drawCurrentAndMax = function(current, max, x, y,
-                                                   width, color1, color2) {
-    if (Yanfly.Param.VHGShowMax) {
-      Window_Base.prototype.drawCurrentAndMax.call(this, current, max,
-        x, y, width, color1, color2);
-    } else {
-      var align = Yanfly.Param.VHGShowHP ? 'right' : 'center';
-      var text = Yanfly.Util.toGroup(current);
-      this.changeTextColor(color1);
-      this.drawText(text, x, y, width, align);
-    }
-};
-*/
-/*
-Window_DispHPGauge.prototype.gaugeHeight = function() {
-    if (!this._battler) return Window_Base.prototype.gaugeHeight.call(this);
-    return this._battler.hpGaugeHeight();
-};
-*/
 
 })();
